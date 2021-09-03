8f31368665ef14ea9e7347312b4663e897463f37
rklgaming <90053019+rklgaming@users.noreply.github.com>
2021-09-03 22:52:27 +0800

    Update rkl.sql

--- rkl.sql
+++ rkl.sql
@@ -1,8 +1,17 @@
 USE `es_extended`;
 
 INSERT INTO `datastore` (name, label, shared) VALUES
-	('user_ears', 'Ears', 0),
-	('user_glasses', 'Glasses', 0),
-	('user_helmet', 'Helmet', 0),
-	('user_mask', 'Mask', 0)
-;
+('user_arms', 'Arms', 0),
+('user_bag', 'Bags', 0),
+('user_bags', 'Bags', 0),
+('user_bracelets', 'Bracelets', 0),
+('user_chain', 'Chain', 0),
+('user_ears', 'Ears', 0),
+('user_glasses', 'Glasses', 0),
+('user_helmet', 'Helmet', 0),
+('user_mask', 'Mask', 0),
+('user_pants', 'Pant', 0),
+('user_shoes', 'Shoes', 0),
+('user_torso', 'Torso', 0),
+('user_tshirt', 'Tshirt', 0),
+('user_watches', 'Watches', 0);
